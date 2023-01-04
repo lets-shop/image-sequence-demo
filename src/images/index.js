ae0082b31b44cdc41d4a7a33d43604e5aa4efe85
Yatharth Manocha <yathman97@gmail.com>
2023-01-05 02:38:51 +0530

    demo init

---
+++ src/images/index.js
@@ -0,0 +1,603 @@
+import A0 from "./0000.jpg";
+import A1 from "./0001.jpg";
+import A2 from "./0002.jpg";
+import A3 from "./0003.jpg";
+import A4 from "./0004.jpg";
+import A5 from "./0005.jpg";
+import A6 from "./0006.jpg";
+import A7 from "./0007.jpg";
+import A8 from "./0008.jpg";
+import A9 from "./0009.jpg";
+
+import A10 from "./0010.jpg";
+import A11 from "./0011.jpg";
+import A12 from "./0012.jpg";
+import A13 from "./0013.jpg";
+import A14 from "./0014.jpg";
+import A15 from "./0015.jpg";
+import A16 from "./0016.jpg";
+import A17 from "./0017.jpg";
+import A18 from "./0018.jpg";
+import A19 from "./0019.jpg";
+
+import A20 from "./0020.jpg";
+import A21 from "./0021.jpg";
+import A22 from "./0022.jpg";
+import A23 from "./0023.jpg";
+import A24 from "./0024.jpg";
+import A25 from "./0025.jpg";
+import A26 from "./0026.jpg";
+import A27 from "./0027.jpg";
+import A28 from "./0028.jpg";
+import A29 from "./0029.jpg";
+
+import A30 from "./0030.jpg";
+import A31 from "./0031.jpg";
+import A32 from "./0032.jpg";
+import A33 from "./0033.jpg";
+import A34 from "./0034.jpg";
+import A35 from "./0035.jpg";
+import A36 from "./0036.jpg";
+import A37 from "./0037.jpg";
+import A38 from "./0038.jpg";
+import A39 from "./0039.jpg";
+
+import A40 from "./0040.jpg";
+import A41 from "./0041.jpg";
+import A42 from "./0042.jpg";
+import A43 from "./0043.jpg";
+import A44 from "./0044.jpg";
+import A45 from "./0045.jpg";
+import A46 from "./0046.jpg";
+import A47 from "./0047.jpg";
+import A48 from "./0048.jpg";
+import A49 from "./0049.jpg";
+
+import A50 from "./0050.jpg";
+import A51 from "./0051.jpg";
+import A52 from "./0052.jpg";
+import A53 from "./0053.jpg";
+import A54 from "./0054.jpg";
+import A55 from "./0055.jpg";
+import A56 from "./0056.jpg";
+import A57 from "./0057.jpg";
+import A58 from "./0058.jpg";
+import A59 from "./0059.jpg";
+
+import A60 from "./0060.jpg";
+import A61 from "./0061.jpg";
+import A62 from "./0062.jpg";
+import A63 from "./0063.jpg";
+import A64 from "./0064.jpg";
+import A65 from "./0065.jpg";
+import A66 from "./0066.jpg";
+import A67 from "./0067.jpg";
+import A68 from "./0068.jpg";
+import A69 from "./0069.jpg";
+
+import A70 from "./0070.jpg";
+import A71 from "./0071.jpg";
+import A72 from "./0072.jpg";
+import A73 from "./0073.jpg";
+import A74 from "./0074.jpg";
+import A75 from "./0075.jpg";
+import A76 from "./0076.jpg";
+import A77 from "./0077.jpg";
+import A78 from "./0078.jpg";
+import A79 from "./0079.jpg";
+
+import A80 from "./0080.jpg";
+import A81 from "./0081.jpg";
+import A82 from "./0082.jpg";
+import A83 from "./0083.jpg";
+import A84 from "./0084.jpg";
+import A85 from "./0085.jpg";
+import A86 from "./0086.jpg";
+import A87 from "./0087.jpg";
+import A88 from "./0088.jpg";
+import A89 from "./0089.jpg";
+
+import A90 from "./0090.jpg";
+import A91 from "./0091.jpg";
+import A92 from "./0092.jpg";
+import A93 from "./0093.jpg";
+import A94 from "./0094.jpg";
+import A95 from "./0095.jpg";
+import A96 from "./0096.jpg";
+import A97 from "./0097.jpg";
+import A98 from "./0098.jpg";
+import A99 from "./0099.jpg";
+
+import A100 from "./0100.jpg";
+import A101 from "./0101.jpg";
+import A102 from "./0102.jpg";
+import A103 from "./0103.jpg";
+import A104 from "./0104.jpg";
+import A105 from "./0105.jpg";
+import A106 from "./0106.jpg";
+import A107 from "./0107.jpg";
+import A108 from "./0108.jpg";
+import A109 from "./0109.jpg";
+
+import A110 from "./0110.jpg";
+import A111 from "./0111.jpg";
+import A112 from "./0112.jpg";
+import A113 from "./0113.jpg";
+import A114 from "./0114.jpg";
+import A115 from "./0115.jpg";
+import A116 from "./0116.jpg";
+import A117 from "./0117.jpg";
+import A118 from "./0118.jpg";
+import A119 from "./0119.jpg";
+
+import A120 from "./0120.jpg";
+import A121 from "./0121.jpg";
+import A122 from "./0122.jpg";
+import A123 from "./0123.jpg";
+import A124 from "./0124.jpg";
+import A125 from "./0125.jpg";
+import A126 from "./0126.jpg";
+import A127 from "./0127.jpg";
+import A128 from "./0128.jpg";
+import A129 from "./0129.jpg";
+
+import A130 from "./0130.jpg";
+import A131 from "./0131.jpg";
+import A132 from "./0132.jpg";
+import A133 from "./0133.jpg";
+import A134 from "./0134.jpg";
+import A135 from "./0135.jpg";
+import A136 from "./0136.jpg";
+import A137 from "./0137.jpg";
+import A138 from "./0138.jpg";
+import A139 from "./0139.jpg";
+
+import A140 from "./0140.jpg";
+import A141 from "./0141.jpg";
+import A142 from "./0142.jpg";
+import A143 from "./0143.jpg";
+import A144 from "./0144.jpg";
+import A145 from "./0145.jpg";
+import A146 from "./0146.jpg";
+import A147 from "./0147.jpg";
+import A148 from "./0148.jpg";
+import A149 from "./0149.jpg";
+
+import A150 from "./0150.jpg";
+import A151 from "./0151.jpg";
+import A152 from "./0152.jpg";
+import A153 from "./0153.jpg";
+import A154 from "./0154.jpg";
+import A155 from "./0155.jpg";
+import A156 from "./0156.jpg";
+import A157 from "./0157.jpg";
+import A158 from "./0158.jpg";
+import A159 from "./0159.jpg";
+
+import A160 from "./0160.jpg";
+import A161 from "./0161.jpg";
+import A162 from "./0162.jpg";
+import A163 from "./0163.jpg";
+import A164 from "./0164.jpg";
+import A165 from "./0165.jpg";
+import A166 from "./0166.jpg";
+import A167 from "./0167.jpg";
+import A168 from "./0168.jpg";
+import A169 from "./0169.jpg";
+
+import A170 from "./0170.jpg";
+import A171 from "./0171.jpg";
+import A172 from "./0172.jpg";
+import A173 from "./0173.jpg";
+import A174 from "./0174.jpg";
+import A175 from "./0175.jpg";
+import A176 from "./0176.jpg";
+import A177 from "./0177.jpg";
+import A178 from "./0178.jpg";
+import A179 from "./0179.jpg";
+
+import A180 from "./0180.jpg";
+import A181 from "./0181.jpg";
+import A182 from "./0182.jpg";
+import A183 from "./0183.jpg";
+import A184 from "./0184.jpg";
+import A185 from "./0185.jpg";
+import A186 from "./0186.jpg";
+import A187 from "./0187.jpg";
+import A188 from "./0188.jpg";
+import A189 from "./0189.jpg";
+
+import A190 from "./0190.jpg";
+import A191 from "./0191.jpg";
+import A192 from "./0192.jpg";
+import A193 from "./0193.jpg";
+import A194 from "./0194.jpg";
+import A195 from "./0195.jpg";
+import A196 from "./0196.jpg";
+import A197 from "./0197.jpg";
+import A198 from "./0198.jpg";
+import A199 from "./0199.jpg";
+
+import A200 from "./0200.jpg";
+import A201 from "./0201.jpg";
+import A202 from "./0202.jpg";
+import A203 from "./0203.jpg";
+import A204 from "./0204.jpg";
+import A205 from "./0205.jpg";
+import A206 from "./0206.jpg";
+import A207 from "./0207.jpg";
+import A208 from "./0208.jpg";
+import A209 from "./0209.jpg";
+
+import A210 from "./0210.jpg";
+import A211 from "./0211.jpg";
+import A212 from "./0212.jpg";
+import A213 from "./0213.jpg";
+import A214 from "./0214.jpg";
+import A215 from "./0215.jpg";
+import A216 from "./0216.jpg";
+import A217 from "./0217.jpg";
+import A218 from "./0218.jpg";
+import A219 from "./0219.jpg";
+
+import A220 from "./0220.jpg";
+import A221 from "./0221.jpg";
+import A222 from "./0222.jpg";
+import A223 from "./0223.jpg";
+import A224 from "./0224.jpg";
+import A225 from "./0225.jpg";
+import A226 from "./0226.jpg";
+import A227 from "./0227.jpg";
+import A228 from "./0228.jpg";
+import A229 from "./0229.jpg";
+
+import A230 from "./0230.jpg";
+import A231 from "./0231.jpg";
+import A232 from "./0232.jpg";
+import A233 from "./0233.jpg";
+import A234 from "./0234.jpg";
+import A235 from "./0235.jpg";
+import A236 from "./0236.jpg";
+import A237 from "./0237.jpg";
+import A238 from "./0238.jpg";
+import A239 from "./0239.jpg";
+
+import A240 from "./0240.jpg";
+import A241 from "./0241.jpg";
+import A242 from "./0242.jpg";
+import A243 from "./0243.jpg";
+import A244 from "./0244.jpg";
+import A245 from "./0245.jpg";
+import A246 from "./0246.jpg";
+import A247 from "./0247.jpg";
+import A248 from "./0248.jpg";
+import A249 from "./0249.jpg";
+
+import A250 from "./0250.jpg";
+import A251 from "./0251.jpg";
+import A252 from "./0252.jpg";
+import A253 from "./0253.jpg";
+import A254 from "./0254.jpg";
+import A255 from "./0255.jpg";
+import A256 from "./0256.jpg";
+import A257 from "./0257.jpg";
+import A258 from "./0258.jpg";
+import A259 from "./0259.jpg";
+
+import A261 from "./0261.jpg";
+import A260 from "./0260.jpg";
+import A262 from "./0262.jpg";
+import A263 from "./0263.jpg";
+import A264 from "./0264.jpg";
+import A265 from "./0265.jpg";
+import A266 from "./0266.jpg";
+import A267 from "./0267.jpg";
+import A268 from "./0268.jpg";
+import A269 from "./0269.jpg";
+
+import A270 from "./0270.jpg";
+import A271 from "./0271.jpg";
+import A272 from "./0272.jpg";
+
+export const AirPods = Array.of(
+  A0,
+  A1,
+  A2,
+  A3,
+  A4,
+  A5,
+  A6,
+  A7,
+  A8,
+  A9,
+
+  A10,
+  A11,
+  A12,
+  A13,
+  A14,
+  A15,
+  A16,
+  A17,
+  A18,
+  A19,
+
+  A20,
+  A21,
+  A22,
+  A23,
+  A24,
+  A25,
+  A26,
+  A27,
+  A28,
+  A29,
+
+  A30,
+  A31,
+  A32,
+  A33,
+  A34,
+  A35,
+  A36,
+  A37,
+  A38,
+  A39,
+
+  A40,
+  A41,
+  A42,
+  A43,
+  A44,
+  A45,
+  A46,
+  A47,
+  A48,
+  A49,
+
+  A50,
+  A51,
+  A52,
+  A53,
+  A54,
+  A55,
+  A56,
+  A57,
+  A58,
+  A59,
+
+  A60,
+  A61,
+  A62,
+  A63,
+  A64,
+  A65,
+  A66,
+  A67,
+  A68,
+  A69,
+
+  A70,
+  A71,
+  A72,
+  A73,
+  A74,
+  A75,
+  A76,
+  A77,
+  A78,
+  A79,
+
+  A80,
+  A81,
+  A82,
+  A83,
+  A84,
+  A85,
+  A86,
+  A87,
+  A88,
+  A89,
+
+  A90,
+  A91,
+  A92,
+  A93,
+  A94,
+  A95,
+  A96,
+  A97,
+  A98,
+  A99,
+
+  A100,
+  A101,
+  A102,
+  A103,
+  A104,
+  A105,
+  A106,
+  A107,
+  A108,
+  A109,
+
+  A110,
+  A111,
+  A112,
+  A113,
+  A114,
+  A115,
+  A116,
+  A117,
+  A118,
+  A119,
+
+  A120,
+  A121,
+  A122,
+  A123,
+  A124,
+  A125,
+  A126,
+  A127,
+  A128,
+  A129,
+
+  A130,
+  A131,
+  A132,
+  A133,
+  A134,
+  A135,
+  A136,
+  A137,
+  A138,
+  A139,
+
+  A140,
+  A141,
+  A142,
+  A143,
+  A144,
+  A145,
+  A146,
+  A147,
+  A148,
+  A149,
+
+  A150,
+  A151,
+  A152,
+  A153,
+  A154,
+  A155,
+  A156,
+  A157,
+  A158,
+  A159,
+
+  A160,
+  A161,
+  A162,
+  A163,
+  A164,
+  A165,
+  A166,
+  A167,
+  A168,
+  A169,
+
+  A170,
+  A171,
+  A172,
+  A173,
+  A174,
+  A175,
+  A176,
+  A177,
+  A178,
+  A179,
+
+  A180,
+  A181,
+  A182,
+  A183,
+  A184,
+  A185,
+  A186,
+  A187,
+  A188,
+  A189,
+
+  A190,
+  A191,
+  A192,
+  A193,
+  A194,
+  A195,
+  A196,
+  A197,
+  A198,
+  A199,
+
+  A200,
+  A201,
+  A202,
+  A203,
+  A204,
+  A205,
+  A206,
+  A207,
+  A208,
+  A209,
+
+  A210,
+  A211,
+  A212,
+  A213,
+  A214,
+  A215,
+  A216,
+  A217,
+  A218,
+  A219,
+
+  A220,
+  A221,
+  A222,
+  A223,
+  A224,
+  A225,
+  A226,
+  A227,
+  A228,
+  A229,
+  
+  A230,
+  A231,
+  A232,
+  A233,
+  A234,
+  A235,
+  A236,
+  A237,
+  A238,
+  A239,
+  
+  A240,
+  A241,
+  A242,
+  A243,
+  A244,
+  A245,
+  A246,
+  A247,
+  A248,
+  A249,
+  
+  A250,
+  A251,
+  A252,
+  A253,
+  A254,
+  A255,
+  A256,
+  A257,
+  A258,
+  A259,
+  
+  A260,
+  A261,
+  A262,
+  A263,
+  A264,
+  A265,
+  A266,
+  A267,
+  A268,
+  A269,
+  
+  A270,
+  A271,
+  A272
+);
